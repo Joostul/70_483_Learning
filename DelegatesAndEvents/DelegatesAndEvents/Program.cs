b25023026c11cb1efb3a3bb1f98df498a1013214
Joost van Uitert <joostvanuitert@outlook.com>
2017-04-15 12:58:27 +0200

    Converted methods to lambdas.

--- DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -8,19 +8,13 @@ namespace DelegatesAndEvents
         {
 
             var worker = new Worker();
-            worker.WorkPerformed += Worker_WorkPerformed;
-            worker.WorkCompleted += delegate(object sender, EventArgs e)
-            {
+            worker.WorkPerformed += (sender, e) => 
+                Console.WriteLine($"On hour {e.Hours.ToString()} {e.WorkType.ToString()} was performed by {sender}.");
+            worker.WorkCompleted += (sender, e) =>
                 Console.WriteLine("Work was completed for this day.");
-            };
             worker.DoWork(8, WorkType.SoftwareEngineering);
 
             Console.ReadKey();
-        }
-
-        private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
-        {
-            Console.WriteLine($"On hour {e.Hours.ToString()} {e.WorkType.ToString()} was performed by {sender}.");
         }
     }
 }
