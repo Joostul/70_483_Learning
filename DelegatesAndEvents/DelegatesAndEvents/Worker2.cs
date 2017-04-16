cfe86db3a15d8c5b83f8b0817cdc31f8c7ea8270
Joost van Uitert <joostvanuitert@outlook.com>
2017-04-16 09:39:33 +0200

    Made second worker with actions and without guidance.

---
+++ DelegatesAndEvents/DelegatesAndEvents/Worker2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesAndEvents
+{
+    public class Worker2
+    {
+        public Action<int, WorkType> WorkPerformed;
+
+        public void DoWork(int hours, WorkType workType)
+        {
+            for (int i = 0; i < hours; i++)
+            {
+                WorkPerformed(i + 1, workType);
+            }
+        }
+    }
+}
